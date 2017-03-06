aa5257644e6eb13080a74e14d3ee2d48d784df06
guilherme <gui_haunter@hotmail.com>
2017-03-06 17:33:02 -0300

    committ

--- src/main/java/br/com/sistema2/controllers/BusinessControllers.java
+++ src/main/java/br/com/sistema2/controllers/BusinessControllers.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
@@ -48,6 +49,7 @@ public class BusinessControllers {
 		return "home2";
 	}
 	
+	@CrossOrigin
 	@RequestMapping("/lista")
 	@ResponseBody
 	public List<Business> lista(){
